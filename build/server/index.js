2b13967e683f3583ca34b14f87288c39e274ef21
Surbhi Bhatia <surbhibhatia1906@gmail.com>
2026-05-31 10:55:56 +0530

    remove empty thoughts route

--- build/server/index.js
+++ build/server/index.js
@@ -1,6 +1,6 @@
-import { D as DevalueError, i as is_primitive, g as get_type, a as is_plain_object, e as enumerable_symbols, s as stringify_key, b as stringify_string, v as valid_array_indices, c as DEV, u as uneval } from './chunks/index2-NM3kSTPa.js';
-import { w as with_request_store, t as text_decoder, b as base64_decode, r as root, d as decode_pathname, n as normalize_path, a as disable_search, c as decode_params, v as validate_layout_server_exports, e as validate_layout_exports, f as validate_page_server_exports, g as validate_page_exports, h as text_encoder$1, i as resolve, m as make_trackable, j as get_relative_path, k as base64_encode } from './chunks/root-Cywuih8l.js';
-import { r as readable, w as writable } from './chunks/index-DC7klWi7.js';
+import { D as DevalueError, i as is_primitive, g as get_type, a as is_plain_object, e as enumerable_symbols, s as stringify_key, b as stringify_string, v as valid_array_indices, c as DEV, u as uneval } from './chunks/index2-gZO4EKvZ.js';
+import { w as with_request_store, t as text_decoder, b as base64_decode, r as root, d as decode_pathname, n as normalize_path, a as disable_search, c as decode_params, v as validate_layout_server_exports, e as validate_layout_exports, f as validate_page_server_exports, g as validate_page_exports, h as text_encoder$1, i as resolve, m as make_trackable, j as get_relative_path, k as base64_encode } from './chunks/root--a-rjf3e.js';
+import { r as readable, w as writable } from './chunks/index-nPKUl6n6.js';
 
 /** @import { StandardSchemaV1 } from '@standard-schema/spec' */
 
@@ -1574,7 +1574,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "htgoja"
+  version_hash: "17kom4b"
 };
 async function get_hooks() {
   let handle;
--- build/server/manifest.js
+++ build/server/manifest.js
@@ -10,22 +10,22 @@ return {
 	assets: new Set([".DS_Store","favicon.svg","flower.svg","fonts/TT Jenevers Trial Light.otf","fonts/TT Jenevers Trial Medium Italic.otf","fonts/TT Jenevers Trial Medium.otf","fonts/TT Jenevers Trial Regular.otf","images/.DS_Store","images/30-day-chart-challenge-doubt-slide.jpg","images/30daychartchallenge-2026/30-day-chart-challenge-preview.gif","images/30daychartchallenge-2026/30-day-chart-challenge-preview.mp4","images/30daychartchallenge-2026/day1-part-to-whole.png","images/30daychartchallenge-2026/day10-popculture.png","images/30daychartchallenge-2026/day11-physical.jpeg","images/30daychartchallenge-2026/day12-flowing-data.png","images/30daychartchallenge-2026/day13-ecosystems.png","images/30daychartchallenge-2026/day14-trade.png","images/30daychartchallenge-2026/day15-correlation.png","images/30daychartchallenge-2026/day16-causation.png","images/30daychartchallenge-2026/day17-remake.png","images/30daychartchallenge-2026/day18-unicef.png","images/30daychartchallenge-2026/day19-evolution.png","images/30daychartchallenge-2026/day2-pictogram.mp4","images/30daychartchallenge-2026/day20-globalchange.png","images/30daychartchallenge-2026/day21-historical.png","images/30daychartchallenge-2026/day22-newtool-revised.png","images/30daychartchallenge-2026/day23-seasons.png","images/30daychartchallenge-2026/day24-south-china-morning-post.png","images/30daychartchallenge-2026/day25-space.png","images/30daychartchallenge-2026/day26-trend.png","images/30daychartchallenge-2026/day27-animation.mp4","images/30daychartchallenge-2026/day28-modeling.png","images/30daychartchallenge-2026/day29-monochrome.png","images/30daychartchallenge-2026/day3-mosaic.png","images/30daychartchallenge-2026/day30-global-health-data-exchange.png","images/30daychartchallenge-2026/day4-slope.mp4","images/30daychartchallenge-2026/day5-experimental.png","images/30daychartchallenge-2026/day6-reporters-without-borders.png","images/30daychartchallenge-2026/day7-multiscale.png","images/30daychartchallenge-2026/day8-circular.png","images/30daychartchallenge-2026/day9-wealth.png","images/30daychartchallenge-2026/thumbs/day1-part-to-whole.jpg","images/30daychartchallenge-2026/thumbs/day10-popculture.jpg","images/30daychartchallenge-2026/thumbs/day11-physical.jpg","images/30daychartchallenge-2026/thumbs/day12-flowing-data.jpg","images/30daychartchallenge-2026/thumbs/day13-ecosystems.jpg","images/30daychartchallenge-2026/thumbs/day14-trade.jpg","images/30daychartchallenge-2026/thumbs/day15-correlation.jpg","images/30daychartchallenge-2026/thumbs/day16-causation.jpg","images/30daychartchallenge-2026/thumbs/day17-remake.jpg","images/30daychartchallenge-2026/thumbs/day18-unicef.jpg","images/30daychartchallenge-2026/thumbs/day19-evolution.jpg","images/30daychartchallenge-2026/thumbs/day2-pictogram.jpg","images/30daychartchallenge-2026/thumbs/day20-globalchange.jpg","images/30daychartchallenge-2026/thumbs/day21-historical.jpg","images/30daychartchallenge-2026/thumbs/day22-newtool-revised.jpg","images/30daychartchallenge-2026/thumbs/day23-seasons.jpg","images/30daychartchallenge-2026/thumbs/day24-south-china-morning-post.jpg","images/30daychartchallenge-2026/thumbs/day25-space.jpg","images/30daychartchallenge-2026/thumbs/day26-trend.jpg","images/30daychartchallenge-2026/thumbs/day27-animation.jpg","images/30daychartchallenge-2026/thumbs/day28-modeling.jpg","images/30daychartchallenge-2026/thumbs/day29-monochrome.jpg","images/30daychartchallenge-2026/thumbs/day3-mosaic.jpg","images/30daychartchallenge-2026/thumbs/day30-global-health-data-exchange.jpg","images/30daychartchallenge-2026/thumbs/day4-slope.jpg","images/30daychartchallenge-2026/thumbs/day5-experimental.jpg","images/30daychartchallenge-2026/thumbs/day6-reporters-without-borders.jpg","images/30daychartchallenge-2026/thumbs/day7-multiscale.jpg","images/30daychartchallenge-2026/thumbs/day8-circular.jpg","images/30daychartchallenge-2026/thumbs/day9-wealth.jpg","images/data-vis-dispatch-01.png","images/data-vis-dispatch-02.png","images/flourish-chart-challenge.png","images/global-partnership-finalist.png","images/indiaspend-ias.png","images/kontinentalist_bollywood.png","images/lede-gurgaon.png","images/lede-met-cats.png","images/mint-bollywood-remakes.png","images/mint-fiscal-deficit.png","images/mint-nobel-prize.png","images/mint-rbi.png","images/og-30-day-chart-challenge-2026.png","images/revisuallabs_arkavathy.png","images/sabew-nikkei-winner.png","images/talk-the-other-interface.png","images/the-pudding-cup-winner.png","images/thibi_ejnacademy.png","images/tmc-passport.png","images/training-thibi-ejn.png","images/words-of-wordle.png","world-map.svg"]),
 	mimeTypes: {".svg":"image/svg+xml",".otf":"font/otf",".jpg":"image/jpeg",".gif":"image/gif",".mp4":"video/mp4",".png":"image/png",".jpeg":"image/jpeg"},
 	_: {
-		client: {start:"_app/immutable/entry/start.vns5L3Jv.js",app:"_app/immutable/entry/app.DEBtq4Wh.js",imports:["_app/immutable/entry/start.vns5L3Jv.js","_app/immutable/chunks/BBDMJiC1.js","_app/immutable/chunks/B8_qDSkM.js","_app/immutable/chunks/DDI9mzuK.js","_app/immutable/chunks/DsU2P2FH.js","_app/immutable/chunks/Bze-GV9_.js","_app/immutable/entry/app.DEBtq4Wh.js","_app/immutable/chunks/DDI9mzuK.js","_app/immutable/chunks/DsU2P2FH.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/B8_qDSkM.js","_app/immutable/chunks/CzeZhf9a.js","_app/immutable/chunks/D-EvJEQT.js","_app/immutable/chunks/Nd9GYR4W.js","_app/immutable/chunks/BR0h39s4.js","_app/immutable/chunks/Bze-GV9_.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.XlXGQ5VA.js",app:"_app/immutable/entry/app.BKQ-Ix5S.js",imports:["_app/immutable/entry/start.XlXGQ5VA.js","_app/immutable/chunks/BbucmZWv.js","_app/immutable/chunks/CELcJ3Ad.js","_app/immutable/chunks/CRWcTHQl.js","_app/immutable/chunks/0IyOi8kv.js","_app/immutable/chunks/BJBxP7N5.js","_app/immutable/entry/app.BKQ-Ix5S.js","_app/immutable/chunks/CRWcTHQl.js","_app/immutable/chunks/0IyOi8kv.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/CELcJ3Ad.js","_app/immutable/chunks/DME8-kaT.js","_app/immutable/chunks/De_aJ3fJ.js","_app/immutable/chunks/g-yMpXWH.js","_app/immutable/chunks/DZLINI36.js","_app/immutable/chunks/BJBxP7N5.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
-			__memo(() => import('./chunks/0-B4fMcyKX.js')),
-			__memo(() => import('./chunks/1-CUimBZ6N.js')),
-			__memo(() => import('./chunks/2-CsnPiShw.js')),
-			__memo(() => import('./chunks/3-MDRLEZ6f.js')),
-			__memo(() => import('./chunks/4-fN0blI4H.js')),
-			__memo(() => import('./chunks/5-CFrsbjO5.js')),
-			__memo(() => import('./chunks/6-Turpz52S.js')),
-			__memo(() => import('./chunks/7-DMdHh6u8.js')),
-			__memo(() => import('./chunks/8-BKMTgDso.js')),
-			__memo(() => import('./chunks/9-BqIMtheN.js')),
-			__memo(() => import('./chunks/10-Dv-D53K4.js')),
-			__memo(() => import('./chunks/11-D70rSuyi.js')),
-			__memo(() => import('./chunks/14-DKNg5IWu.js')),
-			__memo(() => import('./chunks/15-BE0LFEXs.js'))
+			__memo(() => import('./chunks/0-DTkOZ9dY.js')),
+			__memo(() => import('./chunks/1-Z0X0HpSs.js')),
+			__memo(() => import('./chunks/2-IBkRBfmg.js')),
+			__memo(() => import('./chunks/3-B6VC-TYB.js')),
+			__memo(() => import('./chunks/4-CWQgTeCZ.js')),
+			__memo(() => import('./chunks/5-g7eOO5ws.js')),
+			__memo(() => import('./chunks/6-CdMTCYay.js')),
+			__memo(() => import('./chunks/7-CZObXU-z.js')),
+			__memo(() => import('./chunks/8-CXwbr952.js')),
+			__memo(() => import('./chunks/9-BMCNtzIJ.js')),
+			__memo(() => import('./chunks/10-Nrzz6d_w.js')),
+			__memo(() => import('./chunks/11-DkFmRkz1.js')),
+			__memo(() => import('./chunks/12-CsDg-6jd.js')),
+			__memo(() => import('./chunks/13-Be_stNtF.js'))
 		],
 		remotes: {
 			
@@ -137,7 +137,7 @@ return {
 				endpoint: null
 			}
 		],
-		prerendered_routes: new Set(["/thoughts","/vizardry/30-day-chart-challenge-2026","/vizardry/30-day-chart-challenge-2026/__data.json","/vizardry/30-day-map-challenge-2026","/vizardry/30-day-map-challenge-2026/__data.json","/thoughts/colour-is-not-decoration","/thoughts/finding-patterns-in-noise"]),
+		prerendered_routes: new Set(["/vizardry/30-day-chart-challenge-2026","/vizardry/30-day-chart-challenge-2026/__data.json","/vizardry/30-day-map-challenge-2026","/vizardry/30-day-map-challenge-2026/__data.json"]),
 		matchers: async () => {
 			
 			return {  };
@@ -147,7 +147,7 @@ return {
 }
 })();
 
-const prerendered = new Set(["/thoughts","/vizardry/30-day-chart-challenge-2026","/vizardry/30-day-chart-challenge-2026/__data.json","/vizardry/30-day-map-challenge-2026","/vizardry/30-day-map-challenge-2026/__data.json","/thoughts/colour-is-not-decoration","/thoughts/finding-patterns-in-noise"]);
+const prerendered = new Set(["/vizardry/30-day-chart-challenge-2026","/vizardry/30-day-chart-challenge-2026/__data.json","/vizardry/30-day-map-challenge-2026","/vizardry/30-day-map-challenge-2026/__data.json"]);
 
 const base = "";
 
